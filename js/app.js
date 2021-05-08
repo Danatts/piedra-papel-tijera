e2e6fa6fca586d53b64c9851c09e565fc52250df
Danatts <danielatts97@hotmail.com>
2021-05-07 23:13:33 -0500

    Versión 1.0

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-let contadorJug = 0;
-let contadorMaq = 0;
+let num_rondas = parseInt(prompt('¿A cuántas victorias quieres jugar?'));
+
+let contador_jug = 0;
+let contador_maq = 0;
 
 function eleccionMaquina() {
 
@@ -29,19 +31,27 @@ function encontrarResultado(elec_jugador, elec_maquina) {
     let diferencia = num_jugador - num_maquina;
 
     if (resultados_ganadores.includes(diferencia)) {
-        contadorJug = contadorJug + 1;
+        contador_jug = contador_jug + 1;
         resultado = 1;
     } else if (resultados_perdedores.includes(diferencia)) {
-        contadorMaq = contadorMaq + 1;
+        contador_maq = contador_maq + 1;
         resultado = 2;
     } else {
         resultado = 0;
     }
 
-    return [resultado, contadorJug, contadorMaq];
+    return [resultado, contador_jug, contador_maq];
 }
 
-function plasmarResultado(resultado, jugador, maquina) {  
+function cambiarColor(maquina){
+    document.getElementById(maquina + "m").style.background = "firebrick";
+}
+
+function reestablecerColor(maquina){
+    document.getElementById(maquina + "m").style.background = "white";
+}
+
+function plasmarResultado(resultado, jugador, maquina) {
     switch (resultado[0]) {
         case 1:
             document.getElementById("elecJugador").style.color = "green";
@@ -62,9 +72,33 @@ function plasmarResultado(resultado, jugador, maquina) {
     document.getElementById("resultado").innerHTML = `${resultado[1]} - ${resultado[2]}`
 }
 
+function verificarResultado(punt_jugador, punt_maquina){
+    let jugar_denuevo = Boolean;
+    if (punt_jugador == num_rondas || punt_maquina == num_rondas) {
+        document.getElementById("piedra").setAttribute('disabled', true);
+        document.getElementById("papel").setAttribute('disabled', true);
+        document.getElementById("tijera").setAttribute('disabled', true);
+        if (punt_jugador == num_rondas) {
+            jugar_denuevo = confirm('Tú ganas \n¿Quiéres jugar de nuevo?');            
+        }else{
+            jugar_denuevo = confirm('Gano yo \n¿Quiéres jugar de nuevo?');            
+        }
+    }
+    return jugar_denuevo;
+}
+
+
 function main(id_clickeado) {
     let jugador = id_clickeado;
     let maquina = eleccionMaquina();
     let resultado = encontrarResultado(jugador, maquina);
     plasmarResultado(resultado, jugador, maquina);
+    let jugar_denuevo = verificarResultado(resultado[1], resultado[2]);
+    cambiarColor(maquina);
+    setInterval(() => {
+        reestablecerColor(maquina);
+    }, 180);
+    if (jugar_denuevo == true) {
+        window.location.reload();
+    }
 }
